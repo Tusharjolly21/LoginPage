e6b7c6ffd29ba2ef31882a1639a27f80f3a8f6aa
Tushar Jolly <68219099+Tusharjolly21@users.noreply.github.com>
2021-07-10 13:15:59 +0530

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private Button button;
+    private Button button2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = findViewById(R.id.button);
+        button2 = findViewById(R.id.button2);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "You are logging in!!", Toast.LENGTH_SHORT).show();
+            }
+        });
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "You are signing up!!!", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
